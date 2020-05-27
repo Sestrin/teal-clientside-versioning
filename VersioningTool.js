f799902c3b05b5a0b1fb949bd4dc7055612b5637
Stephen Estrin <stephen@Stephens-MacBook-Pro.local>
2020-05-27 14:57:18 -0700

    fixed bug in getcalendar function

--- VersioningTool.js
+++ VersioningTool.js
@@ -253,37 +253,37 @@ function getCalendarNumber(env) {
     var env_string = "";
     for(i in env) env_string += env[i].toString();
     switch(env_string) {
-        case: "0000"
+        case "0000":
             return 0;
-        case: "0001"
+        case "0001":
             return 1;
-        case: "0010"
+        case "0010":
             return 2;
-        case: "0100"
+        case "0100":
             return 3;
-        case: "1000"
+        case "1000":
             return 4;
-        case: "1001"
+        case "1001":
             return 5;
-        case: "1010"
+        case "1010":
             return 6;
-        case: "1100"
+        case "1100":
             return 7;
-        case: "0101"
+        case "0101":
             return 8;
-        case: "0110"
+        case "0110":
             return 9;
-        case: "0011"
+        case "0011":
             return 10;
-        case: "1110"
+        case "1110":
             return 11;
-        case: "1101"
+        case "1101":
             return 12;
-        case: "1011"
+        case "1011":
             return 13;
-        case: "0111"
+        case "0111":
             return 14;
-        case: "1111"
+        case "1111":
             return 15;
     }
 }
